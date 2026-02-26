f9c41ab0438140ca5b8dfd025b77498d60de9d83
Gerardinojm <josecharri123@gmail.com>
2026-02-26 19:31:20 -0400

    estructura de datos y operadores

--- nuevo.py
+++ nuevo.py
@@ -1,1 +1,116 @@
-print ("Hola mundo")
+# Variable en Python
+name = "Gerar" #string
+age = 24 #integer
+Height = 1.72 #float
+is_student = True #boolean
+
+# mlas practicas
+x = 10
+
+number1 = 5
+number2 = 3
+
+# suma
+result_sum = number1 + number2
+
+# print("La suma es:", result_sum)
+
+# print(f"El resultado de la resta es: {number1 - number2}")
+
+# modulo o residuo
+result_modulo = number1 % number2
+
+
+# Operadores de comparación
+# mayor que >
+# menor que <
+# igual a ==
+# diferente de !=
+# mayor o igual que >=
+# menor o igual que <=
+
+# condicional simple
+# age = 18
+# if age >= 20:
+#     print("Eres mayor de edad")
+# else:
+#     print("Eres menor de edad")
+
+# Condicional anidado
+# age = 18
+# weekday = "Viernes"
+
+# if age >= 20:
+#     if weekday == "Viernes":
+#         print("Eres mayor de edad y es viernes")
+#     else:
+#         print("Eres mayor de edad pero no es viernes")
+# else:
+#     print("Eres menor de edad")
+
+#     # Condicionales en cadena
+# age = 21
+# weekday = "Lunes"
+
+# if age >= 20:
+#     if weekday == "Viernes":
+#         print("Eres mayor de edad y es viernes")
+#     elif weekday == "Sabado":
+#         print("Eres mayor de edad y es sabado")
+#     elif weekday == "Domingo":
+#         print("Eres mayor de edad y es domingo")
+#     else:
+#         print("Eres mayor de edad pero no es fin de semana")
+# else:
+#     print("Eres menor de edad")
+
+# Operadores comparacion
+# age = 21
+# weekday = "Lunes"
+
+# if (age >= 20) and ((weekday == "Viernes") or (weekday == "Sabado")):
+#         print("Eres mayor de edad y es viernes")
+# else:
+#     print("Eres menor de edad")
+
+# estructuras de datos
+# greeting = "Hola, ¿cómo estás?" #string
+# number1 = 10 #integer
+# number_float = 3.14 #float
+# is_student = False #boolean
+
+# Listas
+fruits = ["manzana", "banana", "naranja", 10, 3.14, True]
+names = [
+    "Gerar",
+    "Maria",
+    "Juan",
+    "Ana"]
+
+# names.pop()
+
+# add new names to the list
+# names.append("Carlos")
+# print(names[-3])
+
+# tupla
+# coordinates = (10.0, 20.0, 30.0)
+
+# print(coordinates[0])
+
+# names[0] = "Carlos"
+# print(names[0])
+
+# coordinates[0] = 15.0
+# print(coordinates[0])
+
+# diccionario
+person = {
+    "name": "Gerar",
+    "age": 24,
+    "height": 1.72,
+    "is_student": True,
+    "grades": [8, 9, 10]
+}
+
+print(person["name"])
